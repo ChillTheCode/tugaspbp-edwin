14be005bf59a508549b6ae84aae1c0dcdca274c5
ChillTheCode <edwin.daniel@ui.ac.id>
2023-09-13 11:30:14 +0700

    <asoy>

--- main/models.py
+++ main/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    angkatan = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Edwin',
+        'class': 'PBP E',
+        'jumlah_angkatan': '4'
     }
 
     return render(request, "main.html", context)
